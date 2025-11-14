17152bf84aaf213251031d5af610b172da64cfc4
fabiooliveir <fboliveiran@gmail.com>
2025-11-14 11:32:25 -0300

    Adiciona Google Tag Manager na página Seguro Automóveis

--- resources/views/seguro-automoveis.blade.php
+++ resources/views/seguro-automoveis.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WVMW584Q');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Seguro Automóveis - Anjo Azul Corretora de Seguros</title>
@@ -72,6 +79,10 @@
     </style>
 </head>
 <body class="bg-white text-[#002653] font-sans antialiased overflow-x-hidden">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVMW584Q"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- Header Horizontal Fixo -->
     <header class="fixed top-0 left-0 right-0 bg-white text-[#002653] shadow-lg z-50 w-full">
         <div class="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl w-full">
